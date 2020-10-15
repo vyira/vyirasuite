0b1abcfe3470b1d47c3a05caec1b0bfefff44cfc
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2020-10-15 22:38:04 +0530

    Fixed MVC issue

--- ui-app/src/main/java/com/vyira/vyirasuite/uiapp/controller/DefaultController.java
+++ ui-app/src/main/java/com/vyira/vyirasuite/uiapp/controller/DefaultController.java
@@ -4,14 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DefaultController {
     private final Logger Log = LoggerFactory.getLogger(DefaultController.class);
 
     @RequestMapping("/")
-    public String index() {
+    public ModelAndView index() {
         Log.debug("Root Page Requested");
-        return "index";
+        return new ModelAndView("index.html");
     }
 }
