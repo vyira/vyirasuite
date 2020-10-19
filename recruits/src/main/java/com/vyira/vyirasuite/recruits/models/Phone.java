2491987d6c079f452942e0d6a936dbe078087f03
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2020-10-19 22:39:52 +0530

    Models Added

---
+++ recruits/src/main/java/com/vyira/vyirasuite/recruits/models/Phone.java
@@ -0,0 +1,13 @@
+package com.vyira.vyirasuite.recruits.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Phone {
+    public String type;
+    public String number;
+}
