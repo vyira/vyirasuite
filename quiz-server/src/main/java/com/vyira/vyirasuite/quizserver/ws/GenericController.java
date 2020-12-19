c58ae0ed1780453df14bae8e7de93afdc8347473
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2020-12-19 06:03:02 +0530

    Controller

--- quiz-server/src/main/java/com/vyira/vyirasuite/quizserver/ws/GenericController.java
+++ quiz-server/src/main/java/com/vyira/vyirasuite/quizserver/ws/GenericController.java
@@ -1,6 +1,5 @@
 package com.vyira.vyirasuite.quizserver.ws;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
