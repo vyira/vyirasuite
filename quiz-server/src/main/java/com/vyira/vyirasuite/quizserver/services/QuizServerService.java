4204382e427764cb60b98a5d865ba2e382e10f74
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2020-12-21 09:06:26 +0530

    Modified for Middleware service

--- quiz-server/src/main/java/com/vyira/vyirasuite/quizserver/services/QuizServerService.java
+++ quiz-server/src/main/java/com/vyira/vyirasuite/quizserver/services/QuizServerService.java
@@ -1,15 +1,37 @@
 package com.vyira.vyirasuite.quizserver.services;
 
+import com.vyira.vyirasuite.quizserver.impl.QuizServerImpl;
+import com.vyira.vyirasuite.quizserver.models.Question;
 import com.vyira.vyirasuite.quizserver.models.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class QuizServerService {
-    public User getUser(String id){
-        return new User();
+    QuizServerImpl quizServer;
+
+    public User getUser(String id) {
+        log.debug(String.format("getUser(%s)", id));
+        return quizServer.getUser();
     }
 
-    public void addUser(Object user) {
+    public void addUser(User user) {
+        log.debug(String.format("addUser: %s", user.toString()));
+        quizServer.addUser(user);
+    }
 
+    public Question getQuestion(String id) {
+        log.debug(String.format("getQuestion(%s)", id));
+        return quizServer.getQuestion(id);
+    }
+
+    public List<Question> getQuestions(String quizId) {
+        log.debug(String.format("getQuestions(%s)", quizId));
+        return quizServer.getQuestions(quizId);
     }
 }
