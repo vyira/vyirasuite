94e1df56956b4424d3a2259fdcc12ffd6a4d130c
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2020-12-21 09:03:51 +0530

    Better Skeleton Creation

--- quiz-server/src/main/java/com/vyira/vyirasuite/quizserver/impl/QuizServerImpl.java
+++ quiz-server/src/main/java/com/vyira/vyirasuite/quizserver/impl/QuizServerImpl.java
@@ -1,11 +1,29 @@
 package com.vyira.vyirasuite.quizserver.impl;
 
+import com.vyira.vyirasuite.quizserver.models.Question;
+import com.vyira.vyirasuite.quizserver.models.User;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Component
 public class QuizServerImpl implements Serializable {
     private final long serialUID = 1L;
 
+    public User getUser() {
+        return null;
+    }
+
+    public void addUser(User user) {
+
+    }
+
+    public Question getQuestion(String id) {
+        return null;
+    }
+
+    public List<Question> getQuestions(String id) {
+        return null;
+    }
 }
