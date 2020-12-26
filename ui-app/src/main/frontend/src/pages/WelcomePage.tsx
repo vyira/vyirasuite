afb57ef463bcc8780e716732a4981eff2db24590
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2020-12-26 17:24:44 +0530

    WelcomePage.tsx

--- ui-app/src/main/frontend/src/pages/WelcomePage.tsx
+++ ui-app/src/main/frontend/src/pages/WelcomePage.tsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from "axios";
+import {Spinner} from '@fluentui/react'
 
 const WelcomePage: React.FC = () => {
-    return <div>Welcome to Vyira Software Solutions</div>
+    const [pageDetails, setPageDetails] = useState({pageTitle: "", pageDescription: "", loaded:false})
+    if (!pageDetails.loaded) {
+        fetch(`/api/v1/pageDetails?pageTitle=true&pageDescription=true`)
+            .then(response => response.json())
+            .then(data => setPageDetails({pageTitle: data.pageTitle, pageDescription: data.pageDescription, loaded:true}))
+    }
+    console.log(pageDetails)
+    return pageDetails ? (
+        <div>
+            <div>{pageDetails.pageTitle}</div>
+            <div>{pageDetails.pageDescription}</div>
+        </div>
+    ) : (
+        <div>
+            <Spinner label="Seriously, still loading... 😀" ariaLive="assertive" labelPosition="top"/>
+        </div>
+    )
 }
 export default WelcomePage
