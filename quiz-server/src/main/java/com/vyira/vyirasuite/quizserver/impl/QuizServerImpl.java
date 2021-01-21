cff8ddfaae449315be29527262cf57d0975341a9
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2021-01-21 21:53:17 +0530

    Impl Update

--- quiz-server/src/main/java/com/vyira/vyirasuite/quizserver/impl/QuizServerImpl.java
+++ quiz-server/src/main/java/com/vyira/vyirasuite/quizserver/impl/QuizServerImpl.java
@@ -1,10 +1,12 @@
 package com.vyira.vyirasuite.quizserver.impl;
 
 import com.google.common.base.Strings;
+import com.vyira.vyirasuite.quizserver.exceptions.NotFoundException;
 import com.vyira.vyirasuite.quizserver.models.Question;
+import com.vyira.vyirasuite.quizserver.models.Quiz;
 import com.vyira.vyirasuite.quizserver.models.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
@@ -14,27 +16,36 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class QuizServerImpl {
-    MongoTemplate mongoTemplate;
+    MongoOperations mongoOperations;
 
     public User getUser() {
         return null;
     }
 
     public void addUser(User user) {
-
     }
 
     public Question getQuestion(String id) {
         if (Strings.isNullOrEmpty(id)) {
             Query query = new Query();
             query.addCriteria(Criteria.where("_id").is(id));
-            return mongoTemplate.findOne(query, Question.class);
+            return mongoOperations.findOne(query, Question.class);
         } else {
             throw new IllegalArgumentException();
         }
     }
 
-    public List<Question> getQuestions(String id) {
-        return null;
+    public List<Question> getQuestions(String quizId) throws NotFoundException {
+        Query quizQuery = new Query(Criteria.where("_id").is(quizId));
+        Quiz quiz = mongoOperations.findOne(quizQuery, Quiz.class);
+        List<String> questionIdList;
+        if (quiz != null) {
+            questionIdList = quiz.getQuestionIds();
+            Query questionQuery = new Query(Criteria.where("_is").all(questionIdList));
+            return mongoOperations.find(questionQuery, Question.class);
+        }
+        {
+            throw new NotFoundException(String.format("Error in Finding Questions for quizId = %s", quizId));
+        }
     }
 }
