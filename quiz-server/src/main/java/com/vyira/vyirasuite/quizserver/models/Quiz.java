a2227f7b8a73a661b6712b70b5c6a930e8bdd0c4
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2021-01-21 21:50:23 +0530

    DataType Change
    Changing String Array to List for questionIds;

--- quiz-server/src/main/java/com/vyira/vyirasuite/quizserver/models/Quiz.java
+++ quiz-server/src/main/java/com/vyira/vyirasuite/quizserver/models/Quiz.java
@@ -8,18 +8,20 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(value = "quizList")
 public class Quiz implements Serializable {
+    private static final long serialVersionUID = -3394779289727820256L;
 
     @Id
     public String id;
     public String quizName;
     public String description;
-    public String[] questionIds;
+    public List<String> questionIds;
     public Date startTime;
     public long duration;
     public String createdBy;
